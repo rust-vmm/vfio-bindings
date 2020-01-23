735588f06e9743d933033e8a173e797e20ba4cb9
Samuel Ortiz <sameo@linux.intel.com>
2020-01-23 14:35:43 +0100

    fam: Add FAM wrappers for vfio_irq_set
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 // Copyright 2019 Intel Corporation. All Rights Reserved.
 // SPDX-License-Identifier: (BSD-3-Clause OR Apache-2.0)
 
+#[cfg(feature = "fam-wrappers")]
+#[macro_use]
+extern crate vmm_sys_util;
+
+#[cfg(feature = "fam-wrappers")]
+mod fam_wrappers;
+
 #[cfg(feature = "vfio-v5_0_0")]
 mod bindings_v5_0_0;
 
@@ -14,4 +21,7 @@ pub mod bindings {
 
     #[cfg(not(feature = "vfio-v5_0_0"))]
     pub use super::bindings_v5_0_0::*;
+
+    #[cfg(feature = "fam-wrappers")]
+    pub use super::fam_wrappers::*;
 }
